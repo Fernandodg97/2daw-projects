bb9a18f2984b2cbf46707c925e0e3e40b8b51d27
Fernandodg97 <ferdiaz1997@gmail.com>
2024-10-10 20:46:16 +0200

    feat: colocar botones en horizontal en mitad izquierda de la pantalla; añadir div box en la mitad derecha para mostrar el cambio de color

--- color_picker/built/index.js
+++ color_picker/built/index.js
@@ -1,27 +1,7 @@
 "use strict";
-function switchBg(color) {
-    let chosenColor = "white";
-    switch (color) {
-        case "redBt":
-            chosenColor = "red";
-            break;
-        case "yellowBt":
-            chosenColor = "yellow";
-            break;
-        case "greenBt":
-            chosenColor = "green";
-            break;
-        case "blueBt":
-            chosenColor = "blue";
-            break;
-        default:
-            break;
-    }
-    document.body.style.backgroundColor = chosenColor;
-}
-// let redButton: HTMLButtonElement = document.getElementById('redBt') as HTMLButtonElement;
-// redButton.addEventListener("click", () => switchBg('R'));
 let buttons = document.getElementsByTagName("button");
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", () => switchBg(buttons[i].id));
+    buttons[i].addEventListener("click", () => {
+        document.getElementById("box").style.backgroundColor = buttons[i].id;
+    });
 }
--- color_picker/src/ts/index.ts
+++ color_picker/src/ts/index.ts
@@ -1,29 +1,8 @@
-function switchBg(color: string){
-    let chosenColor: string = "white";
-    switch (color) {
-        case "redBt":
-            chosenColor = "red";
-            break;
-        case "yellowBt":
-            chosenColor = "yellow";
-            break;
-        case "greenBt":
-            chosenColor = "green";
-            break;
-        case "blueBt":
-            chosenColor = "blue";
-            break;
-        default:
-            break;
-    }
-    document.body.style.backgroundColor = chosenColor;
-}
-
-// let redButton: HTMLButtonElement = document.getElementById('redBt') as HTMLButtonElement;
-// redButton.addEventListener("click", () => switchBg('R'));
-
 let buttons: HTMLCollectionOf<HTMLButtonElement> = document.getElementsByTagName("button");
 
 for(let i = 0; i < buttons.length; i++){
-    buttons[i].addEventListener("click", () => switchBg(buttons[i].id));
+    buttons[i].addEventListener("click", () =>{
+        document.getElementById("box")!.style.backgroundColor = buttons[i].id;
+    });
 }
+
