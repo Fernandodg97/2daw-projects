001103cbd832b655f6b716012cf244517c87357b
Fernandodg97 <ferdiaz1997@gmail.com>
2024-10-10 19:01:21 +0200

    feat: añadir botones rojo, amarillo, verde y azul; implementar EventListener en el botón rojo

--- color_picker/src/ts/index.ts
+++ color_picker/src/ts/index.ts
@@ -1,3 +1,24 @@
-function switchBg(){
-    document.body.style.backgroundColor = "blue";
+function switchBg(color: string){
+    let chosenColor: string = "white";
+    switch (color) {
+        case "R":
+            chosenColor = "red";
+            break;
+        case "Y":
+            chosenColor = "yellow";
+            break;
+        case "G":
+            chosenColor = "green";
+            break;
+        case "B":
+            chosenColor = "blue";
+            break;
+        default:
+            break;
+    }
+    document.body.style.backgroundColor = chosenColor;
 }
+
+let redButton: HTMLButtonElement = document.getElementById('redButton') as HTMLButtonElement;
+redButton.addEventListener("click", () => switchBg('R'));
+
