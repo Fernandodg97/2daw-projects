232359f1a5737c7de407138a1a37fc3b82d0ccb7
Fernandodg97 <ferdiaz1997@gmail.com>
2024-10-14 20:35:42 +0200

    try: Se añade mouseenter

--- color_picker/built/index.js
+++ color_picker/built/index.js
@@ -2,6 +2,9 @@
 let buttons = document.getElementsByTagName("button");
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", () => {
-        document.getElementById("box").style.backgroundColor = buttons[i].id;
+        document.getElementById("box").style.backgroundColor = buttons[i].value;
+    });
+    buttons[i].addEventListener("mouseenter", () => {
+        document.getElementById("box").style.backgroundColor = buttons[i].value;
     });
 }
--- color_picker/src/ts/index.ts
+++ color_picker/src/ts/index.ts
@@ -2,7 +2,10 @@ let buttons: HTMLCollectionOf<HTMLButtonElement> = document.getElementsByTagName
 
 for(let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener("click", () =>{
-        document.getElementById("box")!.style.backgroundColor = buttons[i].id;
+        document.getElementById("box")!.style.backgroundColor = buttons[i].value;
+    });
+    buttons[i].addEventListener("mouseenter", () =>{
+        document.getElementById("box")!.style.backgroundColor = buttons[i].value;
     });
 }
 
